2f709e6cdb5b9124f624f7ccf26e9fab720754d6
David Lindahl <davidalindahl@gmail.com>
2018-06-14 11:30:41 -0700

    removed lingering variables that werent doing anyone any good! :muscle

--- src/component/AllColorsList/index.js
+++ src/component/AllColorsList/index.js
@@ -1,10 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import SingleColorDetail from '../SingleColorDetail';
 import ColorCard from '../ColorCard';
-import colorsArray from '../../utils/colorGenerator';
-import { withRouter } from 'react-router';
-
 
 class AllColorsList extends React.Component {
   constructor(props){
